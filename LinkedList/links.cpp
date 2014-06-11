a3b577b65e13af336dc68f2b0a794e2bdf789973
Akshat <akshat.gupta@guavus.com>
2014-06-11 10:22:58 +0530

    Added Linked List

---
+++ LinkedList/links.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "links.h"
+using namespace std;
+
+ int main(){
+
+ 	List list;
+ 	list.append(10);
+ 	list.append(20);
+ 	list.append(30);
+ 	list.append(40);
+ 	list.append(50);
+ 	list.print();
+
+
+
+ 	return 0;
+ }
