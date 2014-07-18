81a93c4016f60c8a2fc5b8798acfeac4a614ccbd
Akshat <akshat.gupta@guavus.com>
2014-07-18 15:09:47 +0530

    added AVL trees

---
+++ AVL/avl.h
@@ -0,0 +1,147 @@
+#ifndef AVL_H
+#define AVL_H
+#include <iostream>
+using namespace std;
+
+template <typename Comparable>
+class AVLNode{
+
+public:
+	
+	int height;
+	AVLNode<Comparable> * left;
+	AVLNode<Comparable> * right;
+	Comparable data;
+	AVLNode(Comparable newData, AVLNode<Comparable> * l, AVLNode<Comparable> * r):left(l),right(r),data(newData){}
+		
+};
+
+template <typename Comparable>
+class AVLTree{
+
+private:
+	
+	AVLNode<Comparable> * root;
+	
+public:
+	
+	AVLTree();
+	AVLNode<Comparable> * getRoot();
+	void insert(const Comparable & x);
+	AVLNode<Comparable> * insert(const Comparable & x, AVLNode<Comparable> * curr);
+	AVLNode<Comparable> * rotateSingleWithLeftChild(AVLNode<Comparable> * k2);
+	AVLNode<Comparable> * rotateSingleWithRightChild(AVLNode<Comparable> * k2);	
+	AVLNode<Comparable> * rotateDoubleWithLeftChild(AVLNode<Comparable> * k3);
+	AVLNode<Comparable> * rotateDoubleWithRightChild(AVLNode<Comparable> * k3);
+	int getHeight(AVLNode<Comparable> * curr);
+	int max(int a, int b);
+	void printTreeInOrder(AVLNode<Comparable> * curr);
+		
+};
+
+template <typename Comparable>
+AVLTree<Comparable>::AVLTree(){
+	root = NULL;
+}
+
+template <typename Comparable>
+AVLNode<Comparable> * AVLTree<Comparable>::getRoot(){
+	return root;
+}
+
+template <typename Comparable>
+int AVLTree<Comparable>::getHeight(AVLNode<Comparable> * curr){
+	return (curr == NULL) ? -1 : curr->height;
+}
+
+template <typename Comparable>
+int AVLTree<Comparable>::max(int a, int b){
+	return (a > b) ? a : b;
+}
+
+template <typename Comparable>
+void AVLTree<Comparable>::printTreeInOrder(AVLNode<Comparable> * curr){
+	if(curr == NULL){
+		return;
+	}
+	else {
+		printTreeInOrder(curr->left);
+		cout << curr->data << " ";
+		printTreeInOrder(curr->right);
+	}
+}
+
+template <typename Comparable>
+AVLNode<Comparable> * AVLTree<Comparable>::rotateSingleWithLeftChild(AVLNode<Comparable> * k2){
+	AVLNode<Comparable> * k1 = k2->left;
+	k2->left = k1->right;
+	k1->right = k2;
+	k1->height = max(getHeight(k1->left), getHeight(k1->right)) + 1;
+	k2->height = max(getHeight(k2->left), getHeight(k2->right)) + 1;
+	k2 = k1;
+	return k2;
+}
+
+template <typename Comparable>
+AVLNode<Comparable> * AVLTree<Comparable>::rotateSingleWithRightChild(AVLNode<Comparable> * k2){
+	AVLNode<Comparable> * k1 = k2->right;
+	k2->right = k1->left;
+	k1->left = k2;
+	k1->height = max(getHeight(k1->left), getHeight(k1->right)) + 1;
+	k2->height = max(getHeight(k2->left), getHeight(k2->right)) + 1;
+	k2 = k1;
+	return k2;
+}
+
+template <typename Comparable>
+AVLNode<Comparable> * AVLTree<Comparable>::rotateDoubleWithLeftChild(AVLNode<Comparable> * k3){
+	k3->left = rotateSingleWithRightChild(k3->left);
+	return rotateSingleWithLeftChild(k3);
+}
+
+template <typename Comparable>
+AVLNode<Comparable> * AVLTree<Comparable>::rotateDoubleWithRightChild(AVLNode<Comparable> * k3){
+	k3->right = rotateSingleWithLeftChild(k3->right);
+	return rotateSingleWithRightChild(k3);
+}
+
+template <typename Comparable>
+void AVLTree<Comparable>::insert(const Comparable & x){
+	root = insert(x, root);
+}
+
+template <typename Comparable>
+AVLNode<Comparable> * AVLTree<Comparable>::insert(const Comparable & x, AVLNode<Comparable> * curr){
+	if(curr == NULL){
+		curr = new AVLNode<Comparable> (x, NULL, NULL);
+	}
+	else if(x < curr->data){
+		curr->left = insert(x, curr->left);
+		if((getHeight(curr->left) - getHeight(curr->right)) == 2){
+			if(x < curr->left->data){
+				curr = rotateSingleWithLeftChild(curr);
+			} else {
+				curr = rotateDoubleWithLeftChild(curr);
+			}
+		}
+	}
+	else if(x > curr->data){
+		curr->right = insert(x, curr->right);
+		if((getHeight(curr->right) - getHeight(curr->left)) == 2){
+			if(x > curr->right->data){
+				curr = rotateSingleWithRightChild(curr);
+			} else {
+				curr = rotateDoubleWithRightChild(curr);
+			}
+		}
+	}
+	else ;
+	curr->height = max(getHeight(curr->left), getHeight(curr->right)) + 1;
+	return curr;
+}
+
+
+
+
+#endif
+
---
+++ AVL/main.cpp
@@ -0,0 +1,13 @@
+#include "avl.h"
+using namespace std;
+#include <iostream>
+
+int main(){
+	AVLTree<int> t;
+	t.insert(10);
+	t.insert(15);
+	t.insert(25);
+	t.insert(45);
+	t.insert(55);	
+	t.printTreeInOrder(t.getRoot());
+}
